897462b9a1e1ee67963f834a6ba71b93f4936e4e
Juan Carlos Lores Barrero <jloresb@jloresb.local>
2018-05-10 22:20:59 -0500

    simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
 import { FirstModule } from './firstModule/firstModule.module';
-
 import { SecondModule } from './secondModule/second.module';
+import { Routes, RouterModule } from '@angular/router';
+import {
+  ParentComponent
+} from './firstModule';
+
+import {
+  ExternalComponent
+} from './secondModule';
+
+const appRoutes: Routes = [{
+  path: '', redirectTo: '/parent', pathMatch: 'full'
+},
+{ path: 'parent', component: ParentComponent },
+{ path: 'external', component: ExternalComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -14,7 +27,8 @@ import { SecondModule } from './secondModule/second.module';
   imports: [
     BrowserModule,
     FirstModule,
-    SecondModule
+    SecondModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
